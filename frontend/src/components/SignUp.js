6053a2c32e902abd4ef40476c4e3a1f5e77e4f7a
Eduardo Duarte <dudu_duarte@hotmail.com>
2020-06-10 10:40:16 -0300

    feat(SignUp): inserir campos do formulario de cadastro.

--- frontend/src/components/SignUp.js
+++ frontend/src/components/SignUp.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from 'react';
 
 const SignUp = () => {
-    return (
-        <div>
-            <h1>Sign Up</h1>
-        </div>
-    )
-}
+  return (
+    <div>
+      <h1>Sign Up</h1>
+      <div>
+        <input type='text' placeholder='Nome' />
+      </div>
+      <div>
+        <input type='text' placeholder='Usuario' />
+      </div>
+      <div>
+        <input type='password' placeholder='Senha' />
+      </div>
+      <div>
+        <input type='password' placeholder='Confirmar senha' />
+      </div>
+      <div>
+        <button>Enviar</button>
+      </div>
+    </div>
+  );
+};
 
-export default SignUp
+export default SignUp;
--- frontend/src/components/SignUps.test.js
+++ frontend/src/components/SignUps.test.js
@@ -6,9 +6,44 @@ import SignUp from './SignUp';
 describe('SignUp', () => {
   describe('Layout', () => {
     it('Deve existir o componente', () => {
-        const { container } = render(<SignUp />);
-        const header = container.querySelector('h1');
-        expect(header).toHaveTextContent('Sign Up')
+      const { container } = render(<SignUp />);
+      const header = container.querySelector('h1');
+      expect(header).toHaveTextContent('Sign Up');
     });
+    it('input para enviar com placeholder "Nome"', () => {
+      const { queryByPlaceholderText } = render(<SignUp />);
+      const displayInput = queryByPlaceholderText('Nome');
+      expect(displayInput).toBeInTheDocument();
+    });
+    it('input para enviar com placeholder "Usuario"', () => {
+      const { queryByPlaceholderText } = render(<SignUp />);
+      const displayInput = queryByPlaceholderText('Usuario');
+      expect(displayInput).toBeInTheDocument();
+    });
+    it('input para enviar com placeholder "Senha"', () => {
+      const { queryByPlaceholderText } = render(<SignUp />);
+      const displayInput = queryByPlaceholderText('Senha');
+      expect(displayInput).toBeInTheDocument();
+    });
+    it('input do tipo "Senha"', () => {
+      const { queryByPlaceholderText } = render(<SignUp />);
+      const displayInput = queryByPlaceholderText('Senha');
+      expect(displayInput.type).toBe('password');
+    });
+    it('input para enviar com placeholder "Confirmar senha"', () => {
+      const { queryByPlaceholderText } = render(<SignUp />);
+      const displayInput = queryByPlaceholderText('Confirmar senha');
+      expect(displayInput).toBeInTheDocument();
+    });
+    it('input do tipo "Senha"', () => {
+      const { queryByPlaceholderText } = render(<SignUp />);
+      const displayInput = queryByPlaceholderText('Confirmar senha');
+      expect(displayInput.type).toBe('password');
+    });
+    it('Deve existir o botão de enviar', () => {
+        const { container } = render(<SignUp />);
+        const header = container.querySelector('button');
+        expect(header).toHaveTextContent('Enviar');
+      });
   });
 });
