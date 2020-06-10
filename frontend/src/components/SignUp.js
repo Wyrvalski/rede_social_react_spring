7f8a17b8d3f6bf694bfc11d978ab8edab7cff657
Eduardo Duarte <dudu_duarte@hotmail.com>
2020-06-10 11:47:46 -0300

    feat(SignUp): adicionar formulario ao state.

--- frontend/src/components/SignUp.js
+++ frontend/src/components/SignUp.js
@@ -1,20 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const SignUp = () => {
+  const [formData, setformData] = useState({
+    name: '',
+    username: '',
+    password: '',
+    confirmPassword: ''
+  });
+
+  const { name, username, password, confirmPassword } = formData;
+
+  const onChangeFormData = (e) => {
+    setformData({ ...formData, [e.target.name]: e.target.value });
+  };
+
   return (
     <div>
       <h1>Sign Up</h1>
       <div>
-        <input type='text' placeholder='Nome' />
+        <input
+          type='text'
+          name='name'
+          placeholder='Nome'
+          value={name}
+          onChange={(e) => onChangeFormData(e)}
+        />
       </div>
       <div>
-        <input type='text' placeholder='Usuario' />
+        <input
+          type='text'
+          placeholder='Usuario'
+          name='username'
+          value={username}
+          onChange={(e) => onChangeFormData(e)}
+        />
       </div>
       <div>
-        <input type='password' placeholder='Senha' />
+        <input
+          type='password'
+          placeholder='Senha'
+          name='password'
+          value={password}
+          onChange={(e) => onChangeFormData(e)}
+        />
       </div>
       <div>
-        <input type='password' placeholder='Confirmar senha' />
+        <input type='password' placeholder='Confirmar senha' name='confirmPassword' value={confirmPassword}
+          onChange={(e) => onChangeFormData(e)}/>
       </div>
       <div>
         <button>Enviar</button>
--- frontend/src/components/SignUps.test.js
+++ frontend/src/components/SignUps.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import SignUp from './SignUp';
 
@@ -41,9 +41,53 @@ describe('SignUp', () => {
       expect(displayInput.type).toBe('password');
     });
     it('Deve existir o botão de enviar', () => {
-        const { container } = render(<SignUp />);
-        const header = container.querySelector('button');
-        expect(header).toHaveTextContent('Enviar');
-      });
+      const { container } = render(<SignUp />);
+      const header = container.querySelector('button');
+      expect(header).toHaveTextContent('Enviar');
+    });
+  });
+  describe('intereções', () => {
+    const changeEvent = (content) => {
+      console.log(content);
+      
+        return {
+        target: {
+          value: content
+        }
+      };
+    };
+    it('Deve enviar o input nome para detro do state', () => {
+      const { queryByPlaceholderText } = render(<SignUp />);
+      const displayInput = queryByPlaceholderText('Nome');
+
+      fireEvent.change(displayInput, changeEvent('nome-no-input'));
+
+      expect(displayInput).toHaveValue('nome-no-input');
+    });
+    it('Deve enviar o input Usuario para detro do state', () => {
+      const { queryByPlaceholderText } = render(<SignUp />);
+      const displayInput = queryByPlaceholderText('Usuario');
+
+      fireEvent.change(displayInput, changeEvent('username-no-input'));
+
+      expect(displayInput).toHaveValue('username-no-input');
+    });
+
+    it('Deve enviar o input Senha para detro do state', () => {
+      const { queryByPlaceholderText } = render(<SignUp />);
+      const displayInput = queryByPlaceholderText('Senha');
+
+      fireEvent.change(displayInput, changeEvent('senha-no-input'));
+
+      expect(displayInput).toHaveValue('senha-no-input');
+    });
+    it('Deve enviar o input Confirmar senha para detro do state', () => {
+      const { queryByPlaceholderText } = render(<SignUp />);
+      const displayInput = queryByPlaceholderText('Confirmar senha');
+
+      fireEvent.change(displayInput, changeEvent('confirmar-senha-no-input'));
+
+      expect(displayInput).toHaveValue('confirmar-senha-no-input');
+    });
   });
 });
